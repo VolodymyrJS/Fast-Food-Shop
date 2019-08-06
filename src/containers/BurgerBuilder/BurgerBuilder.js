495b5a8e638f26daa618b1c4a080c4d7980eef64
Vladimir Ponomarenko <p.v.v1313@gmail.com>
2019-08-07 01:18:51 +0300

    Addin more login and components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Helper from '../../HOC/Helper';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      cheese: 0,
+      meat: 0,
+      bacon: 0
+    }
+  };
   render() {
     return (
       <Helper>
-        <div>Burger area</div>
-        <div>Burger parts</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Helper>
     );
   }
