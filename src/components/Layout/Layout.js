edb6b024b391c7e1103fe0d69b260d96bcbb9a8e
Vladimir Ponomarenko <p.v.v1313@gmail.com>
2019-08-10 19:33:54 +0300

    Adding header to the app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Helper from '../../HOC/Helper';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => {
   return (
     <Helper>
-      <div>Toolbar Sidedrawer Backdrop</div>
+      <Toolbar />
       <main className={classes.layoutSetings}>{props.children}</main>
     </Helper>
   );
