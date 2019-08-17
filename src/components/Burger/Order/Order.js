27d4e35113f8a895034769f7af16157802487707
Vladimir Ponomarenko <p.v.v1313@gmail.com>
2019-08-17 23:21:09 +0300

    finished checkout page

--- src/components/Burger/Order/Order.js
+++ src/components/Burger/Order/Order.js
@@ -1,11 +1,34 @@
 import React from "react";
 import classes from "./Order.module.css";
 
-const order = () => (
-  <div className={classes.Order}>
-    <p>Ingredients: salad(1)</p>
-    <p>Price: 5.45</p>
-  </div>
-);
+const order = props => {
+  console.log(props);
+  const ingredients = [];
+
+  for (let ingredientsName in props.ingredients) {
+    ingredients.push({
+      name: ingredientsName,
+      amount: props.ingredients[ingredientsName]
+    });
+    console.log(ingredientsName);
+  }
+
+  const ingredientsOutput = ingredients.map(ig => {
+    return (
+      <span key={ig.name}>
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  //console.log(ingredientsOutput);
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientsOutput}</p>
+      <p>Price: {props.price.toFixed(2)}</p>
+    </div>
+  );
+};
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
 import Order from "../../components/Burger/Order/Order";
+import axios from "../../axios-order";
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(resp => {
+        const fetchedOrders = [];
+        for (let key in resp.data) {
+          fetchedOrders.push({
+            ...resp.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={+order.price}
+          />
+        ))}
       </div>
     );
   }
